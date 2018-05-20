1c9c854bd441eebe87a0f9ce8b46e5268542ba1a
Semennikov Konstantin <Systemko@yandex.ru>
2018-05-21 01:04:02 +0300

    Start project

---
+++ OptimalAllocation/Allocation.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OptimalAllocation
+{
+    public sealed class Allocation
+    {
+        private AllocationParameters _parameters;
+
+        public Allocation(AllocationParameters allocationParameters)
+        {
+            validateAllocationParameters(allocationParameters);
+            _parameters = allocationParameters;
+        }
+
+        public AllocationResults GetAllocation()
+        {
+            var data = _parameters.AllocationData;
+            var intermidiateResults = new List<StepResult>();
+            var totalFunc = _parameters.TotalFunc;
+            StepResult previousStepResult = new StepResult();
+            previousStepResult.F = data[0];
+            for (int i = 1; i < data.Length; i++)
+            {
+                previousStepResult = iterateStepForward(previousStepResult.F, data[i]);
+                intermidiateResults.Add(previousStepResult);
+            }
+
+            var optimalIndexes = iterateBackward(intermidiateResults);
+            var results = new AllocationResults();
+            results.Allocation = new float[optimalIndexes.Length];
+            for (int i = 0; i < optimalIndexes.Length; i++)
+            {
+                results.Allocation[i] = _parameters.AllocationResource[optimalIndexes[i]];
+                results.Total += totalFunc(results.Allocation[i], _parameters.AllocationData[i][optimalIndexes[i]]);
+            }
+            return results;
+        }
+
+        public AllocationParameters Parameters
+        {
+            get { return _parameters; }
+            set {
+                validateAllocationParameters(value);
+                _parameters = value;
+            }
+        }
+
+        private StepResult iterateStepForward(float[] previous, float[] current)
+        {
+            int count = previous.Length + current.Length - 1;
+            if (count > _parameters.ResourceCount)
+            {
+                count = _parameters.ResourceCount;
+            }
+            StepResult result = new StepResult(count);
+            for (int i = 0; i < current.Length; i++)
+            {
+                for (int j = 0; j < previous.Length; j++)
+                {
+                    if (i + j < count)
+                    {
+                        float sum = current[i] + previous[j];
+                        if (_parameters.CriterialSelect(result.F[i + j], sum))
+                        {
+                            result.F[i + j] = sum;
+                            result.x[i + j] = i;
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+            return result;
+        }
+
+        private int[] iterateBackward(List<StepResult> results)
+        {
+            var indexCount = _parameters.AllocationData.Length;
+            var optimalIndexes = new int[indexCount];
+            var lastForwardStep = results[results.Count - 1];
+            var total = _parameters.ResourceCount - 1;
+
+            optimalIndexes[indexCount - 1] = lastForwardStep.x[lastForwardStep.x.Length - 1];
+            
+            for (int i = indexCount - 2; i > 0; i--)
+            {
+                optimalIndexes[i] = results[i - 1].x[total - optimalIndexes[i + 1]];
+                total -= optimalIndexes[i + 1];
+            }
+            optimalIndexes[0] = total - optimalIndexes[1];
+            return optimalIndexes;
+        }
+
+        private void validateAllocationParameters(AllocationParameters parameters)
+        {
+            if (parameters == null) throw new ArgumentException("Allocation parameters can not be null");
+        }
+    }
+
+    public sealed class AllocationParameters 
+    {
+        private readonly int _resourceCount;
+        private readonly float[] _allocationResource;
+        private readonly float[][] _allocationData;
+        private readonly Func<float, float, float> _totalFunc;
+        private readonly Func<float, float, bool> _criterialSelect;
+
+        public AllocationParameters(float[] allocationResource, 
+                                    float[][] allocationData,
+                                    CriteriaType criteriaType,
+                                    Func<float, float, float> totalFunc = null,
+                                    bool isLeadingZerosIncluded = false)
+        {         
+            if (isLeadingZerosIncluded)
+            {
+                _allocationResource = allocationResource;
+                _allocationData = allocationData;
+            }
+            else
+            {
+                _allocationResource = addLeadingZeros(allocationResource);
+                _allocationData = addLeadingZerosToData(allocationData);
+            }
+            int dataCount = 0;
+            _resourceCount = _allocationResource.Length;
+            for (int i = 0; i < _allocationData.Length; i++)
+            {
+                dataCount += _allocationData[i].Length-1;
+            }            
+            if (dataCount < _resourceCount - 1)
+            {
+                _resourceCount = dataCount + 1;
+            }            
+            _totalFunc = totalFunc;
+            if (_totalFunc == null)
+            {
+                _totalFunc = (curResource, curData) => curData;
+            }
+            switch (criteriaType)
+            {
+                case CriteriaType.Min:
+                    _criterialSelect = (oldOne, newOne) =>
+                    {
+                        if (oldOne > newOne || oldOne == 0) return true;
+                        return false;
+                    };
+                    break;
+                case CriteriaType.Max:
+                    _criterialSelect = (oldOne, newOne) =>
+                    {
+                        if (oldOne < newOne) return true;
+                        return false;
+                    };
+                    break;
+            }
+        }
+
+        public int ResourceCount
+        {
+            get { return _resourceCount; }
+        }
+
+        public float[] AllocationResource
+        {
+            get { return _allocationResource; }
+        }
+
+        public float[][] AllocationData
+        {
+            get { return _allocationData; }
+        }
+
+        public Func<float, float, bool> CriterialSelect
+        {
+            get { return _criterialSelect; }
+        }
+
+        public Func<float, float, float> TotalFunc
+        {
+            get { return _totalFunc; }
+        }
+
+        private float[] addLeadingZeros(float[] data)
+        {
+            float[] dataWithZeros = new float[data.Length + 1];
+            Array.ConstrainedCopy(data, 0, dataWithZeros, 1, data.Length);
+            return dataWithZeros;
+        }
+
+        private float[][] addLeadingZerosToData(float[][] allocationData)
+        {
+            float[][] allocationDataWithZeros = new float[allocationData.Length][]; ;
+            for (int i = 0; i < allocationData.Length; i++)
+            {
+                allocationDataWithZeros[i] = addLeadingZeros(allocationData[i]);
+            }
+            return allocationDataWithZeros;
+        }
+    }
+
+    public struct StepResult
+    {
+        public float[] F;
+        public int[] x;
+
+        public StepResult(int count)
+        {
+            F = new float[count];
+            x = new int[count];
+        }
+    }
+
+    public struct AllocationResults
+    {
+        public float[] Allocation;
+        public float Total;
+    }
+
+    public enum CriteriaType
+    {
+        Min,
+        Max
+    }
+}
---
+++ OptimalAllocationTest/GetAllocationTest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OptimalAllocation;
+
+namespace OptimalAllocationTest
+{
+    [TestClass]
+    public class GetAllocationTest
+    {
+        [TestMethod]
+        public void GetAllocation_MaxCriteria_1_Ok()
+        {
+            float[] expectedResult = new float[] { 200, 200, 100, 200 };
+
+            float[] resource = new float[] { 100, 200, 300, 400, 500, 600, 700 };
+            float[] f1 = new float[] { 42, 58, 71, 80, 89, 95, 100 };
+            float[] f2 = new float[] { 30, 49, 63, 68, 69, 65, 60 };
+            float[] f3 = new float[] { 22, 37, 49, 59, 68, 76, 82 };
+            float[] f4 = new float[] { 50, 68, 82, 92, 100, 107, 112 };
+            float[][] data = new float[][] { f1, f2, f3, f4 };
+
+            AllocationParameters parameters = new AllocationParameters(resource, data, CriteriaType.Max);
+            testAllocationResults(parameters, expectedResult, 197F);
+        }
+
+        [TestMethod]
+        public void GetAllocation_MaxCriteria_2_Ok()
+        {
+            float[] expectedResult = new float[] { 0, 1, 4 };
+            float[] resource = new float[] { 1, 2, 3, 4, 5 };
+            float[] f1 = new float[] { 3.22F, 3.57F, 4.12F, 4F, 4.85F };
+            float[] f2 = new float[] { 3.33F, 4.87F, 5.26F, 7.34F, 9.49F };
+            float[] f3 = new float[] { 4.27F, 7.64F, 10.25F, 15.93F, 16.12F };
+            float[][] data = new float[][] { f1, f2, f3 };
+
+            AllocationParameters parameters = new AllocationParameters(resource, data, CriteriaType.Max);
+            testAllocationResults(parameters, expectedResult, 19.26F);
+        }
+
+        [TestMethod]
+        public void GetAllocation_MinCriteria_1_Ok()
+        {
+            float[] expectedResult = new float[] { 7, 0, 1, 5 };
+
+            float[] resource = Enumerable.Range(1, 13).Select(x => (float) x).ToArray(); 
+            float[] f1 = new float[] { 1.08F, 2.04F, 3F, 4F, 5F, 5.82F, 6.79F };
+            float[] f2 = new float[] { 1.04F, 2.02F };
+            float[] f3 = new float[] { 1.03F, 2.02F, 3F, 4F, 5F, 6F, 7F };
+            float[] f4 = new float[] { 1.01F, 2F, 3F, 4F, 4.9F, 6F, 7F };
+            float[][] data = new float[][] { f1, f2, f3, f4 };
+
+            AllocationParameters parameters = new AllocationParameters(resource, data, CriteriaType.Min);
+            testAllocationResults(parameters, expectedResult, 12.72F);
+        }
+
+        private void testAllocationResults(AllocationParameters parameters, float[] expectedResult, float expectedTotal)
+        {
+            Allocation allocation = new Allocation(parameters);
+            AllocationResults result = allocation.GetAllocation();
+            for (int i = 0; i < expectedResult.Length; i++)
+            {
+                Assert.IsTrue(expectedResult[i] == result.Allocation[i]);
+            }
+            Assert.AreEqual(Math.Round(expectedTotal,2), Math.Round(result.Total, 2));
+        }
+    }
+}
